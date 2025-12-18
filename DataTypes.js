93504367872c3ec1852313f0c0581ee9e534b27c
meharban <meharbankassar920@gmail.com>
2025-12-18 18:34:43 +0530

    DataType Basic

---
+++ DataTypes.js
@@ -0,0 +1,50 @@
+//primitive datatype
+
+const { syncIndexes } = require("mongoose");
+
+//?-string
+
+let name="Mohit";
+// console.log(name)
+// console.log(typeof name)
+
+ //?-Number
+
+let age=20;
+
+// console.log(age)
+// console.log(typeof age)
+
+//?-Boolean
+
+let student=true;
+// console.log(student)
+// console.log(typeof student)
+
+//?-null
+
+let employee=null;
+// console.log(employee)
+// console.log(typeof employee)
+
+
+//?-nudefind
+
+let abc;
+// console.log(abc)
+// console.log(typeof abc)
+
+//?-Bigint
+
+let big=478484972823n;
+// console.log(big)
+// console.log(typeof big)
+
+//?-Symbol
+
+let SymbolValue=Symbol("unique");
+// console.log(SymbolValue);
+// console.log(typeof SymbolValue)
+
+
+
---
+++ Variables.js
@@ -0,0 +1,59 @@
+// var num=10;
+// console.log(num)
+
+// var name="Rahul";
+// var name="Vikash";
+
+// console.log(name)
+
+// let name="Meharban";
+//   name="Rahul"
+// console.log(name)
+
+// const name="Rahul";
+// name="Vaksh"
+// console.log(name)
+
+
+// let name="vikash" //?->outside scop
+// {
+//     let name="Rahul";
+//     console.log(name)//?-> let inside blok-Scope
+// }
+// console.log(name)
+
+
+// var name="Vikash";
+// {
+//     var name="Rahul";
+//     console.log(name) //?->var-function Scope
+// }
+// console.log(name)
+
+// const name="Vikash";
+// {
+//     const name="Rahul";
+//     console.log(name) //?->const-  Blok Scope
+// }
+// console.log(name)
+
+// let x=20;
+//  let x=230  //?->redeclare not allow
+// x=100;  //?->reassign is alllow
+// console.log(x)
+
+// const x=20;
+ // const x=30;//?->redeclare not allow
+// x=40; //?->Reassign not allow
+// console.log(x)
+
+// console.log(x)
+// var x=20;  //?->Hoisted-but undefined
+
+
+// console.log(x);
+// let x=20;  //?->Hoisted-not undefined
+
+
+console.log(x);
+const x=20;//?->Hoisted-not undefined
