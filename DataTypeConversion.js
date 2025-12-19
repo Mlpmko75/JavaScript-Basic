93063e3d8adc80bbaf0244e72e2101e6326423cd
meharban <meharbankassar920@gmail.com>
2025-12-19 22:34:13 +0530

    update

---
+++ DataTypeConversion.js
@@ -0,0 +1,30 @@
+//! Data type Conversion
+
+//? 1.Number
+
+// let num="1223";
+// let num=null;
+// let num=undefined;
+// let num=true;
+// let ValueToNumber=Number(num);//? Convert string to Number
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+//! 2. Boolean;
+// let bool=true;
+// let bool=3;
+// let bool="0";
+// let ValueToBoolean=Boolean(bool); //? Convert string to Boolean
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+//! 3. String
+
+// let str="123";
+// let str=true;
+// let str=null;
+// let str=undefined;
+let str=1223;
+let ValueToString=String(str); //?Convert number to string
+console.log(ValueToString);
+console.log(typeof ValueToString);
---
+++ ReferenceDataType.js
@@ -0,0 +1,33 @@
+//!-Refrence Data type
+
+//?->1. Object
+let person={
+    name:"Mohit",
+    age:30,
+    city:"New York"
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+console.log(person);
+console.log(typeof person)
+
+//? 2. Array
+let fruits=["Apple","Banana", "Mango"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits);
+console.log(typeof fruits)
+
+//?-3. Function
+function grret(){
+    console.log("Hello, Mohit");
+}
+console.log(typeof grret)
+grret();
+
+
+
+
+
